d9a32b8096932a4b6550462e8dd37e465f6b307d
Tony Chow <tchow9@gmail.com>
2023-02-23 17:42:15 -0800

    Uploaded code to repo

--- breederFetcher.js
+++ breederFetcher.js
@@ -2,14 +2,19 @@ const request = require('request');
 
 const cmdLineArray = (cmdLine) => {
   return cmdLine.slice(2);
-}
+};
 let array = cmdLineArray(process.argv);
-// console.log(array)
+
 request(`https://api.thecatapi.com/v1/breeds/search?q=${array[0]}`, (error, response, body) => {
-  // console.log('error', error);
-  // console.log('statusCode', response && response.statusCode);
-  // console.log(body)
   const data = JSON.parse(body);
-  // console.log(data)
-  // console.log(typeof data)
-})
+  // console.log(data[0])
+  if (data[0] === undefined) {
+    console.log('error: ', error);
+    console.log('response: ', response && response.statusCode);
+    console.log('Breed does not exist, please try again.');
+  } else {
+    console.log(data[0]['description']);
+  }
+  
+
+});
