93854a0d56556911222210efa0275d1c1715113b
Tony Chow <tchow9@gmail.com>
2023-02-24 18:06:19 -0800

    Removed old commented out codde

--- breederFetcher.js
+++ breederFetcher.js
@@ -1,18 +1,4 @@
 const request = require('request');
-
-// const fetchBreedDescription = function(breedName, callback) {
-//   request(`https://api.thecatapi.com/v1/breeds/search?q=${array[0]}`, (error, response, body) => {
-//     const data = JSON.parse(body);//converts the data received into an object to use instead of a string
-//     if (data[0] === undefined) {//handling errors, if breed or domain is typed incorrectly
-//       console.log('error: ', error);
-//       console.log('response: ', response && response.statusCode);
-//       console.log('Breed does not exist, please try again.');
-//     } else {
-//       console.log(data[0]['description']);//outputs the description
-//     }
-//   });
-// }
-
 const fetchBreedDescription = function(breedName, callback) {
   request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error, response, body) => {
     const data = JSON.parse(body);//converts the data received into an object to use instead of a string
